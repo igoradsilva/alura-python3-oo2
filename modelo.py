c4d0f81d593008b61370d582cf111bbc524f9723
igor <igorsilv@hotmail.com>
2020-11-14 20:50:27 -0300

    Aula 2 Final - Removendo duplicação com herança

--- modelo.py
+++ modelo.py
@@ -1,47 +1,33 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome.title()
+
+    def dar_like(self):
+        self._likes += 1
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
 
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome.title()
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome.title()
-
-    def dar_like(self):
-        self.__likes += 1
-
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
 vingadores.dar_like()
@@ -56,6 +42,5 @@ atlanta.dar_like()
 atlanta.dar_like()
 
 print()
-print(f"Nome: {vingadores.nome}\tAno: {vingadores.ano}\tDuração: {vingadores.duracao}\tLikes:{vingadores.likes}")
-print()
-print(f"Nome: {atlanta.nome}\tAno: {atlanta.ano}\tTemporadas: {atlanta.temporadas}\tLikes:{atlanta.likes}")
+print(f"{vingadores.nome} / {vingadores.ano} / {vingadores.duracao}min / {vingadores.likes} likes")
+print(f"{atlanta.nome} / {atlanta.ano} / {atlanta.temporadas} temporadas / {atlanta.likes} likes")
